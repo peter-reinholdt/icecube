2f0f57a85015972f382d606431203f7c3e2547f8
Peter Reinholdt <peter.reinholdt@gmail.com>
2017-10-21 20:21:00 +0200

    Basic functionality works now

--- grid.py
+++ grid.py
@@ -5,26 +5,26 @@ import horton
 
 
 class griddata(object):
-    def __init__(self, qmfilename, npts=np.array([120, 120, 120]), bufsize=10.0):
+    def __init__(self, qmfilename, npts=np.array([120, 120, 120]), bufsize=5.0):
         #load qm data
         IO                  = horton.IOData.from_file(qmfilename)
-        self.dm             = IO.dm
+        self.dm             = IO.get_dm_full()
         self.icharges       = IO.numbers
         self.fcharges       = IO.numbers.astype(np.float64)
         self.obasis         = IO.obasis
         self.natoms         = len(self.icharges)
         self.coordinates    = IO.coordinates
 
-        #get origin pmin, pmax
-        pmin = np.min(self.coordinates, axis=0) - bufsize
-        pmax = np.max(self.coordinates, axis=0) + bufsize
-
-        origin = pmin
+        self.center_of_charge = np.sum(self.coordinates*self.fcharges[:,np.newaxis], axis=0) / np.sum(self.fcharges)
+        
+        pmin = self.center_of_charge - bufsize
+        pmax = self.center_of_charge + bufsize
+        print(pmin, pmax)
+        self.origin = pmin
 
         self.Nx = npts[0]
         self.Ny = npts[1]
         self.Nz = npts[2]
-        self.origin = np.array([pmin[0], pmin[1], pmin[2]], dtype=np.float64)
         self.xx = (pmax[0] - pmin[0]) / npts[0]
         self.yy = (pmax[1] - pmin[1]) / npts[1]
         self.zz = (pmax[2] - pmin[2]) / npts[2]
@@ -37,9 +37,9 @@ class griddata(object):
         self.xrange  = np.linspace(pmin[0], pmax[0], npts[0])
         self.yrange  = np.linspace(pmin[1], pmax[1], npts[1])
         self.zrange  = np.linspace(pmin[2], pmax[2], npts[2])
-        self.xyzgrid = np.zeros((Nx*Ny*Nz,3), dtype=np.float64)
+        self.xyzgrid = np.zeros((self.Nx*self.Ny*self.Nz,3), dtype=np.float64)
         self.make_xyz_grid()
-        self.data    = np.zeros((Nx*Ny*Nz),   dtype=np.float64)
+        self.data    = np.zeros((self.Nx*self.Ny*self.Nz),   dtype=np.float64)
 
 
 
@@ -49,14 +49,14 @@ class griddata(object):
             for j in range(self.Ny):
                 for k in range(self.Nz):
                     self.xyzgrid[counter, 0] = self.xrange[i]
-                    self.xyzgrid[counter, 1] = self.xrange[j]
-                    self.xyzgrid[counter, 2] = self.xrange[k]
+                    self.xyzgrid[counter, 1] = self.yrange[j]
+                    self.xyzgrid[counter, 2] = self.zrange[k]
                     counter += 1
     
 
     def compute_density(self):
-        self.data = self.compute_grid_esp_dm(self.dm, self.xyzgrid, self.data)
+        self.data = self.obasis.compute_grid_density_dm(self.dm, self.xyzgrid, self.data)
 
 
     def compute_potential(self):
-        self.data = self.compute_grid_esp_dm(self.dm, self.xyzgrid, self.fcharges, self.data)
+        self.data = self.obasis.compute_grid_esp_dm(self.dm, self.xyzgrid, self.fcharges, self.data)
--- icecube.py
+++ icecube.py
@@ -3,18 +3,18 @@ import sys
 import grid
 import cube
 
-if __name__ == __main__():
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: icecube [qmfile]")
         exit()
-    qmfilename = sys.arg[1] 
+    qmfilename = sys.argv[1] 
 
     #write density
     gr = grid.griddata(qmfilename)
     gr.compute_density()
-    cube.writecube("{}_rho.cube".format(qmfilename), gr)
+    cube.write_cube("{}_rho.cube".format(qmfilename), gr)
 
     #write log(density) -- note the base
     gr.data = np.log(gr.data)
-    cube.writecube("{}_logrho.cube".format(qmfilename), gr)
+    cube.write_cube("{}_logrho.cube".format(qmfilename), gr)
 
